eb0c6d182dd0e1233d2d869b4c0ad9afa58398f5
Victória Gamarano Prieto <viqprieto@gmail.com>
2020-11-16 21:49:27 -0300

    splash, onboarding 1 e 2, login, cadastro

--- app/src/main/java/com/digitalhouse/marsgaze/ui/OnboardingActivity.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/ui/OnboardingActivity.kt
@@ -2,11 +2,17 @@ package com.digitalhouse.marsgaze.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.digitalhouse.marsgaze.R
 
 class OnboardingActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+
+        navController = findNavController(R.id.navHostfragment)
     }
 }
