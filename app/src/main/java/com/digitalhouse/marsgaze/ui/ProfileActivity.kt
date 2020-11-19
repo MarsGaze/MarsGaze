04b7ca0abf24e650c258e5932fd55de6ce42d8f1
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-11-19 14:10:57 -0300

    Finalize welcome page and profile page. Although profile page may be subjected to changes such as password change.

---
+++ app/src/main/java/com/digitalhouse/marsgaze/ui/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.digitalhouse.marsgaze.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.digitalhouse.marsgaze.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
