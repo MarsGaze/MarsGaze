93a9228cd29f9666fdd7e2b6a7df39628edaf9d5
Angelica <santosangelicassp@gmail.com>
2020-11-19 10:06:50 -0300

    Criando activity de exception para imagem não encontrada

---
+++ app/src/main/java/com/digitalhouse/marsgaze/ui/ExceptionActivity.kt
@@ -0,0 +1,17 @@
+package com.digitalhouse.marsgaze.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.digitalhouse.marsgaze.R
+import kotlinx.android.synthetic.main.activity_exception.*
+
+class ExceptionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exception)
+
+        iv_backArrow.setOnClickListener {
+            finish()
+        }
+    }
+}
