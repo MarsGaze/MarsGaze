253fc14e3c7b6e9da93a95a57b15e2ddf30425ae
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2020-12-28 16:34:16 -0300

    Add ViewModel to control database related actions

---
+++ app/src/main/java/com/digitalhouse/marsgaze/viewmodels/login/LoginViewModel.kt
@@ -0,0 +1,29 @@
+package com.digitalhouse.marsgaze.viewmodels.login
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.digitalhouse.marsgaze.R
+import com.digitalhouse.marsgaze.controllers.user.Session
+import com.digitalhouse.marsgaze.models.data.User
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LoginViewModel(val session: Session) : ViewModel() {
+    val loginStatus = MutableLiveData<Pair<Boolean, Int>>()
+
+    fun loginUser(user: User) {
+        Dispatchers.IO.dispatch(viewModelScope.coroutineContext) {
+            val result = session.login(user)
+
+            var message = 0
+            if (!result) {
+                message = R.string.userLoginInvalid
+            }
+
+            viewModelScope.launch {
+                loginStatus.value = Pair(result, message)
+            }
+        }
+    }
+}
