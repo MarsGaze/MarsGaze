23aa5768ae4e428092615d371e39601a0c50733e
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2021-01-14 16:58:26 -0300

    Reimplement binding

--- app/src/main/java/com/digitalhouse/marsgaze/ui/CuriosidadesFragment.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/ui/CuriosidadesFragment.kt
@@ -11,19 +11,21 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import com.digitalhouse.marsgaze.R
-import kotlinx.android.synthetic.main.fragment_curiosidades.*
 import com.digitalhouse.marsgaze.databinding.FragmentCuriosidadesBinding
 
 class CuriosidadesFragment : Fragment() {
 
-    private lateinit var binding: FragmentCuriosidadesBinding
+    private var _binding: FragmentCuriosidadesBinding? = null
+
+    private val binding: FragmentCuriosidadesBinding
+    get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentCuriosidadesBinding.inflate(inflater, container, false)
+        _binding = FragmentCuriosidadesBinding.inflate(inflater, container, false)
 
         return binding.root
     }
@@ -33,99 +35,101 @@ class CuriosidadesFragment : Fragment() {
 
         //PARTE HISTÓRICA
 
-        //variaveis de controle para o card que contém o texto HISTORIA
-        val cardHistoria = expandable_text_history
-        val botaoHistoria = expand_button_history
-        val textoHistoria = hidden_text_historia
+        binding.run {
+            //variaveis de controle para o card que contém o texto HISTORIA
+            val cardHistoria = expandableTextHistory
+            val botaoHistoria = expandButtonHistory
+            val textoHistoria = hiddenTextHistoria
 
-        //ação para mostrar o texto Hitória
-        setExpandable(cardHistoria, botaoHistoria, textoHistoria, null)
+            //ação para mostrar o texto Hitória
+            setExpandable(cardHistoria, botaoHistoria, textoHistoria, null)
 
-        //variaveis de controle para o card que contém o texto NOME
-        val cardNome = expandable_text_name
-        val botaoNome = expand_button_name
-        val textoNome = hidden_text_name
+            //variaveis de controle para o card que contém o texto NOME
+            val cardNome = expandableTextName
+            val botaoNome = expandButtonName
+            val textoNome = hiddenTextName
 
-        //ação para mostrar o texto NOME
-        setExpandable(cardNome, botaoNome, textoNome, null)
+            //ação para mostrar o texto NOME
+            setExpandable(cardNome, botaoNome, textoNome, null)
 
 
-        //variaveis de controle para o card que contém o texto CLIMA
-        val cardClima = expandable_text_clima
-        val botaoClima = expand_button_clima
-        val textoClima = hidden_text_clima
+            //variaveis de controle para o card que contém o texto CLIMA
+            val cardClima = expandableTextClima
+            val botaoClima = expandButtonClima
+            val textoClima = hiddenTextClima
 
-        //ação para mostrar o texto CLIMA
-        setExpandable(cardClima, botaoClima, textoClima, null)
+            //ação para mostrar o texto CLIMA
+            setExpandable(cardClima, botaoClima, textoClima, null)
 
-        //variaveis de controle para o card que contém o texto SOLO
-        val cardSolo = expandable_text_solo
-        val botaoSolo = expand_button_solo
-        val textoSolo = hidden_text_solo
+            //variaveis de controle para o card que contém o texto SOLO
+            val cardSolo = expandableTextSolo
+            val botaoSolo = expandButtonSolo
+            val textoSolo = hiddenTextSolo
 
-        //ação para mostrar o texto SOLO
-        setExpandable(cardSolo, botaoSolo, textoSolo, null)
+            //ação para mostrar o texto SOLO
+            setExpandable(cardSolo, botaoSolo, textoSolo, null)
 
-        //variaveis de controle para o card que contém o texto SATELITES
-        val cardSatelites = expandable_text_satelites
-        val botaoSatelites = expand_button_satelites
-        val textoSatelites = hidden_text_satelites
+            //variaveis de controle para o card que contém o texto SATELITES
+            val cardSatelites = expandableTextSatelites
+            val botaoSatelites = expandButtonSatelites
+            val textoSatelites = hiddenTextSatelites
 
-        //ação para mostrar o texto SATELITES
-        setExpandable(cardSatelites, botaoSatelites, textoSatelites, null)
+            //ação para mostrar o texto SATELITES
+            setExpandable(cardSatelites, botaoSatelites, textoSatelites, null)
 
-        //variaveis de controle para o card que contém o texto ATMOSFERA
-        val cardAtmosfera = expandable_text_atmosfera
-        val botaoAtmosfera = expand_button_atmosfera
-        val textoAtmosfera = hidden_text_atmosfera
+            //variaveis de controle para o card que contém o texto ATMOSFERA
+            val cardAtmosfera = expandableTextAtmosfera
+            val botaoAtmosfera = expandButtonAtmosfera
+            val textoAtmosfera = hiddenTextAtmosfera
 
-        //ação para mostrar o texto ATMOSFERA
-        setExpandable(cardAtmosfera, botaoAtmosfera, textoAtmosfera, null)
+            //ação para mostrar o texto ATMOSFERA
+            setExpandable(cardAtmosfera, botaoAtmosfera, textoAtmosfera, null)
 
-        //PARTE DE CURIOSIDADES
+            //PARTE DE CURIOSIDADES
 
-        //variaveis de controle para o card que contém o texto CYDONIA
-        val cardCydonia = expandable_text_cydonia
-        val botaoCydonia = expand_button_cydonia
-        val textoCydonia = hidden_text_cydonia
-        val imgCydonia = img_cydonia
+            //variaveis de controle para o card que contém o texto CYDONIA
+            val cardCydonia = expandableTextCydonia
+            val botaoCydonia = expandButtonCydonia
+            val textoCydonia = hiddenTextCydonia
+            val imgCydonia = imgCydonia
 
-        //ação para mostrar o texto CYDONIA
-        setExpandable(cardCydonia, botaoCydonia, textoCydonia, imgCydonia)
+            //ação para mostrar o texto CYDONIA
+            setExpandable(cardCydonia, botaoCydonia, textoCydonia, imgCydonia)
 
-        //variaveis de controle para o card que contém o texto HAPPY FACE
-        val cardHappyFace = expandable_text_happyFace
-        val botaoHappyFace = expand_button_happyFace
-        val textoHappyFace = hidden_text_happyFace
-        val imgHappyFace = img_happyFace
+            //variaveis de controle para o card que contém o texto HAPPY FACE
+            val cardHappyFace = expandableTextHappyFace
+            val botaoHappyFace = expandButtonHappyFace
+            val textoHappyFace = hiddenTextHappyFace
+            val imgHappyFace = imgHappyFace
 
-        //ação para mostrar o texto HAPPY FACE
-        setExpandable(cardHappyFace, botaoHappyFace, textoHappyFace, imgHappyFace)
+            //ação para mostrar o texto HAPPY FACE
+            setExpandable(cardHappyFace, botaoHappyFace, textoHappyFace, imgHappyFace)
 
-        //variaveis de controle para o card que contém o texto BUTTERFLY
-        val cardButterfly = expandable_text_butterfly
-        val botaoButterfly = expand_button_butterfly
-        val textoButterfly = hidden_text_butterfly
-        val imgButterfly = img_butterfly
+            //variaveis de controle para o card que contém o texto BUTTERFLY
+            val cardButterfly = expandableTextButterfly
+            val botaoButterfly = expandButtonButterfly
+            val textoButterfly = hiddenTextButterfly
+            val imgButterfly = imgButterfly
 
-        //ação para mostrar o texto BUTTERFLY
-        setExpandable(cardButterfly, botaoButterfly, textoButterfly, imgButterfly)
+            //ação para mostrar o texto BUTTERFLY
+            setExpandable(cardButterfly, botaoButterfly, textoButterfly, imgButterfly)
 
-        //variaveis de controle para o card que contém o texto ANEL DE MARTE
-        val cardAnelMarte = expandable_text_anelMarte
-        val botaoAnelMarte = expand_button_anelMarte
-        val textoAnelMarte = hidden_text_anelMarte
+            //variaveis de controle para o card que contém o texto ANEL DE MARTE
+            val cardAnelMarte = expandableTextAnelMarte
+            val botaoAnelMarte = expandButtonAnelMarte
+            val textoAnelMarte = hiddenTextAnelMarte
 
-        //ação para mostrar o texto ANEL DE MARTE
-        setExpandable(cardAnelMarte, botaoAnelMarte, textoAnelMarte, null)
+            //ação para mostrar o texto ANEL DE MARTE
+            setExpandable(cardAnelMarte, botaoAnelMarte, textoAnelMarte, null)
 
-        //variaveis de controle para o card que contém o texto CURIOSIDADES GEOGRAFICAS
-        val cardCuriosidadesGeograficas = expandable_text_curiosidadesGeograficas
-        val botaoCuriosidadesGeograficas = expand_button_curiosidadesGeograficas
-        val textoCuriosidadesGeograficas = hidden_text_curiosidadesGeograficas
+            //variaveis de controle para o card que contém o texto CURIOSIDADES GEOGRAFICAS
+            val cardCuriosidadesGeograficas = expandableTextCuriosidadesGeograficas
+            val botaoCuriosidadesGeograficas = expandButtonCuriosidadesGeograficas
+            val textoCuriosidadesGeograficas = hiddenTextCuriosidadesGeograficas
 
-        //ação para mostrar o texto CURIOSIDADES GEOGRAFICAS
-        setExpandable(cardCuriosidadesGeograficas, botaoCuriosidadesGeograficas, textoCuriosidadesGeograficas, null)
+            //ação para mostrar o texto CURIOSIDADES GEOGRAFICAS
+            setExpandable(cardCuriosidadesGeograficas, botaoCuriosidadesGeograficas, textoCuriosidadesGeograficas, null)
+        }
     }
 
     private fun setExpandable(expandableText: CardView,  expandButton: ImageView, hiddenText: TextView, hiddenImage: CardView?) {
@@ -157,5 +161,10 @@ class CuriosidadesFragment : Fragment() {
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
