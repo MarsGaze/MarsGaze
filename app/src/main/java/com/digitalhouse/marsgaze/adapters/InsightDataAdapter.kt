c68cdb51487529691e7b160e36179ef429e8edfc
Angelica <santosangelicassp@gmail.com>
2021-01-18 13:37:14 -0300

    adicionando layout para quanto não houver dados da API

--- app/src/main/java/com/digitalhouse/marsgaze/adapters/InsightDataAdapter.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/adapters/InsightDataAdapter.kt
@@ -11,31 +11,63 @@ import com.digitalhouse.marsgaze.models.InsightInfo
 
 class InsightDataAdapter(var infoList: ArrayList<InsightInfo> = ArrayList()) : PagerAdapter() {
     var sol: Int = 0
+    private lateinit var view: View
     override fun isViewFromObject(view: View, `object`: Any): Boolean = `object` == view
     override fun getCount(): Int = 2
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val view = LayoutInflater.from(container.context).inflate(
-            R.layout.insight_sol_info, container, false
-        )
-
-        var msg = " aslkjdaksldhalskjdh"
-
-        Log.i("\n\n\n\n\n AOISDASLDH", infoList.toString())
-        Log.i("\n\n\n\n\n 5435465465454", msg)
 
         if (infoList.isEmpty()) {
-            container.addView(view)
+            view = LayoutInflater.from(container.context).inflate(
+                R.layout.insight_exception, container, false )
             return view
         }
 
         val obj = infoList[sol]
 
+        when(position) {
+            0 -> {
+                if (obj.AT.av == "NO_DATA") {
+                    view = LayoutInflater.from(container.context).inflate(
+                        R.layout.insight_exception, container, false
+                    )
+                } else {
+                    view = LayoutInflater.from(container.context).inflate(
+                        R.layout.insight_sol_info, container, false
+                    )
+                    inflateDataLayout(view, position, obj)
+                }
+            }
+            1 -> {
+                if (obj.PRE.av == "NO_DATA") {
+                    view = LayoutInflater.from(container.context).inflate(
+                        R.layout.insight_exception, container, false
+                    )
+                } else {
+                    view = LayoutInflater.from(container.context).inflate(
+                        R.layout.insight_sol_info, container, false
+                    )
+                    inflateDataLayout(view, position, obj)
+                }
+            }
+        }
+
+
+
+        container.addView(view)
+
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+    fun inflateDataLayout(view: View, position: Int, obj: InsightInfo) {
         val media = view.findViewById<TextView>(R.id.tv_media)
         val tv_valorMinima = view.findViewById<TextView>(R.id.tv_valorMinima)
         val tv_valorMaxima = view.findViewById<TextView>(R.id.tv_valorMaxima)
         val tv_amostras = view.findViewById<TextView>(R.id.tv_amostras)
-
 
         when(position) {
             0 -> {
@@ -51,13 +83,5 @@ class InsightDataAdapter(var infoList: ArrayList<InsightInfo> = ArrayList()) : P
                 tv_amostras.text = obj.PRE.ct
             }
         }
-
-        container.addView(view)
-
-        return view
-    }
-
-    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-        container.removeView(`object` as View)
     }
 }
