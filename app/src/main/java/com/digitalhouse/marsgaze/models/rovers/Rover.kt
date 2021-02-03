3ffd9b5b3fb7f3c2418c4f505dcbedddaa523e05
Matheus Vianna <67394504+matheusvianna95@users.noreply.github.com>
2021-02-03 12:59:48 -0300

    Matheusvianna95 (#17)
    * fixing sign out
    
    * botao de logout no menu
    
    * moving sign out from profile fragment into menu
    
    * first implementation of share button
    
    * share button - first inplementation
    
    * share button - second implementation
    
    Co-authored-by: Victória Gamarano Prieto <viqprieto@gmail.com>

--- app/src/main/java/com/digitalhouse/marsgaze/models/rovers/Rover.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/models/rovers/Rover.kt
@@ -1,6 +1,7 @@
 package com.digitalhouse.marsgaze.models.rovers
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 /**
  * Data class that represents the Rover associated with a particular
@@ -12,4 +13,4 @@ data class  Rover(
     @SerializedName("landing_date") val landingDate: String,
     @SerializedName("launch_date") val launchDate: String,
     val status: String
-)
+) : Serializable
--- app/src/main/java/com/digitalhouse/marsgaze/models/rovers/RoverCamera.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/models/rovers/RoverCamera.kt
@@ -1,6 +1,7 @@
 package com.digitalhouse.marsgaze.models.rovers
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
 /**
  * Data class that represents the camera associated with a photo from Mars Rover Photos.
@@ -9,4 +10,4 @@ import com.google.gson.annotations.SerializedName
 data class RoverCamera(
 	@SerializedName("name") val abbrName: String,
 	@SerializedName("full_name") val fullName: String
-)
+) : Serializable
