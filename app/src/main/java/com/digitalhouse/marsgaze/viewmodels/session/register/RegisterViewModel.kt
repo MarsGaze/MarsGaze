03a26fb554020ffcf0b042b680df475a1b04dcb1
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2021-02-07 18:48:05 -0300

    Implement validation of the user data

--- app/src/main/java/com/digitalhouse/marsgaze/viewmodels/session/register/RegisterViewModel.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/viewmodels/session/register/RegisterViewModel.kt
@@ -2,6 +2,7 @@ package com.digitalhouse.marsgaze.viewmodels.session.register
 
 import android.database.sqlite.SQLiteConstraintException
 import android.database.sqlite.SQLiteException
+import android.text.TextUtils
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -14,7 +15,13 @@ import kotlinx.coroutines.launch
 class RegisterViewModel(val session: Session) : ViewModel() {
     val registerStatus = MutableLiveData<Pair<Boolean, Int>>()
 
-    fun registerUser(user: User) {
+    fun registerUser(user: User, confirmPswd: String) {
+        val result = validateUser(user, confirmPswd)
+        if (!result.first) {
+            registerStatus.value = result
+            return
+        }
+
         Dispatchers.IO.dispatch(viewModelScope.coroutineContext) {
             var returnValue: Pair<Boolean, Int> = Pair(true, R.string.userRegistered)
             try {
@@ -32,5 +39,31 @@ class RegisterViewModel(val session: Session) : ViewModel() {
             }
         }
 
+    }
+
+    private fun validateUser(user: User, passwordRpt: String): Pair<Boolean, Int> {
+        return if (user.name.isEmpty() || user.name.isBlank()) {
+            Pair(false, R.string.emptyName)
+        } else if (user.email.isEmpty() || user.email.isBlank()) {
+            Pair(false, R.string.emptyEmail)
+        } else if (user.password.isNullOrBlank() || user.password.isNullOrEmpty()) {
+            Pair(false, R.string.emptyPassword)
+        } else if (passwordRpt.isEmpty() || passwordRpt.isBlank()) {
+            Pair(false, R.string.emptyPasswordRepeat)
+        }else if (!user.email.isEmailValid()) {
+            Pair(false, R.string.invalidEmail)
+        } else if (user.name.length < 2 || user.name.length > 25) {
+            Pair(false, R.string.invalidName)
+        } else if (user.password!!.length < 8 || user.password!!.length > 25) {
+            Pair(false, R.string.invalidPassword)
+        } else if (user.password != passwordRpt) {
+            Pair(false, R.string.invalidPasswordRepeat)
+        }else {
+            Pair(true, 0)
+        }
+    }
+
+    private fun String.isEmailValid(): Boolean {
+        return !TextUtils.isEmpty(this) && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
     }
 }
