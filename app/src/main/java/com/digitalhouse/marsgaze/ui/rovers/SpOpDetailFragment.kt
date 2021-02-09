b466c6033463724322f830ea193e58c71cd28f79
Thinkaboutmin <19597109+Thinkaboutmin@users.noreply.github.com>
2021-02-09 17:20:44 -0300

    Add intent for nasa url in rover details

--- app/src/main/java/com/digitalhouse/marsgaze/ui/rovers/SpOpDetailFragment.kt
+++ app/src/main/java/com/digitalhouse/marsgaze/ui/rovers/SpOpDetailFragment.kt
@@ -1,20 +1,53 @@
 package com.digitalhouse.marsgaze.ui.rovers
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.digitalhouse.marsgaze.R
+import com.digitalhouse.marsgaze.databinding.FragmentCuriosityDetailBinding
+import com.digitalhouse.marsgaze.databinding.FragmentSpOpDetailBinding
 
 class SpOpDetailFragment : Fragment() {
+
+    var _binding: FragmentSpOpDetailBinding? = null
+
+    val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sp_op_detail, container, false)
+        _binding = FragmentSpOpDetailBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.referenceLinkText.setOnClickListener {
+            val intent = browserIntent()
+            startActivity(intent)
+        }
+
+        binding.referenceText.setOnClickListener {
+            val intent = browserIntent()
+            startActivity(intent)
+        }
+    }
+
+    private fun browserIntent(url: String = binding.referenceLinkText.text.toString()): Intent {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        return intent
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
